1b7e01bd997def22f419912e136b01b4342f57f1
changweihua <changweihua@outlook.com>
2026-08-26 21:17:57 +0800

    Reapply "🐞 fix: radial menu transparent areas mouse passthrough"
    This reverts commit 74560320b2e1215a72cb531aa40e324470c5fc09.

--- src/preload/radial.ts
+++ src/preload/radial.ts
@@ -19,4 +19,6 @@ contextBridge.exposeInMainWorld('radialApi', {
   dragStart: (mouseX: number, mouseY: number) => ipcRenderer.send('radial:drag-start', mouseX, mouseY),
   dragMove: (mouseX: number, mouseY: number) => ipcRenderer.send('radial:drag-move', mouseX, mouseY),
   dragEnd: () => ipcRenderer.send('radial:drag-end'),
+  // 透明区域鼠标穿透：交互元素 mouseenter/mouseleave 时动态切换
+  setIgnoreMouseEvents: (ignore: boolean) => ipcRenderer.send('radial:set-ignore-mouse-events', ignore),
 })
